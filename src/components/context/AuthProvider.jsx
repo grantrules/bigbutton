6c33dad94d5c8af60af6d4fa982423951fa04aa1
grantrules <gharding@gmail.com>
2020-09-23 03:21:33 -0400

    cleanup

--- src/components/context/AuthProvider.jsx
+++ src/components/context/AuthProvider.jsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useMutation } from 'urql';
 
-const LOGIN_QUERY = 'mutation Login($email: String, $password: String) { login(email:$email, password:$password) }';
-
-const LOGOUT_QUERY = 'query Logout { logout }';
-
 const AuthContext = React.createContext({
   activeUser: null,
   login: () => {},
@@ -13,23 +9,30 @@ const AuthContext = React.createContext({
   loginFailed: false,
 });
 
+const LOGIN_MUTATION = 'mutation Login($email: String, $password: String) { login(email:$email, password:$password) }';
+const LOGOUT_QUERY = 'query Logout { logout }';
+
+const LOGGED_IN = { activeUser: true, loginFailed: false };
+const LOGGED_OUT = { activeUser: false, loginFailed: false };
+const LOGIN_FAILED = { activeUser: false, loginFailed: true };
+
 function AuthProvider({ activeSession, children }) {
   const [{ activeUser, loginFailed }, setUserState] = useState({
     activeUser: activeSession,
     loginFailed: false,
   });
-  const [/* loginResult */, loginQuery] = useMutation(LOGIN_QUERY);
+  const [/* loginResult */, loginQuery] = useMutation(LOGIN_MUTATION);
   const [/* loginResult */, logoutQuery] = useMutation(LOGOUT_QUERY);
 
   const invalidate = async () => logoutQuery({})
-    .then(() => setUserState({ activeUser: false, loginFailed: false }));
+    .then(() => setUserState(LOGGED_OUT));
 
   const login = async (details) => {
     const { data, error } = await loginQuery(details);
     if (error || data.login === false) {
-      setUserState({ activeUser: false, loginFailed: true });
+      setUserState(LOGIN_FAILED);
     } else {
-      setUserState({ activeUser: true, loginFailed: false });
+      setUserState(LOGGED_IN);
     }
   };
 
