7311121b91a8f1b9f86c4d46373aac87ac89353a
grantrules <gharding@gmail.com>
2020-09-23 03:10:48 -0400

    fine i'll just fix it

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -2,12 +2,12 @@ const bcrypt = require('bcrypt');
 
 const hash = (password) => new Promise(
   (resolve, reject) => bcrypt.hash(password, 10,
-    (err, result) => (err && reject(err)) || resolve(result)),
+    (err, result) => (err ? reject(err) : resolve(result))),
 );
 
 const compare = (password, hashToCompare) => new Promise(
   (resolve, reject) => bcrypt.compare(password, hashToCompare,
-    (err, result) => (err && reject(err)) || resolve(result)),
+    (err, result) => (err ? reject(err) : resolve(result))),
 );
 
 module.exports = { hash, compare };
